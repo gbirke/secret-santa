f46b6ba679526ac0246e65928a56d95cf12732e0
Gabriel Birke <gabriel.birke@gmail.com>
2018-12-05 23:12:51 +0100

    Outline code for other routes

--- src/Controller/SecretSantaController.php
+++ src/Controller/SecretSantaController.php
@@ -1,5 +1,4 @@
 <?php
-// src/Controller/LuckyController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,8 +14,23 @@ class SecretSantaController extends AbstractController
 		$submissions = [];
 
         return $this->render( 'secret-santa/index.html.twig', [
-        	'peope' => $people,
+        	'people' => $people,
 			'submissions' => $submissions
 		]);
     }
+
+    public function storeSubmission() {
+    	//TODO
+		// Store donor id and receiver status (known/unknown) in submissions table in DB
+		// if gift status is known, store receiver in known_receivers table in DB
+		// if submissions length === people length, go to pick route, otherwise display success page
+	}
+
+	public function pickReceivers() {
+    	// TODO
+		// $donors = people - submissions where receiver is known
+		// $receivers = $people - known_receivers
+		// match $donors randomly to $receivers, making sure that donor[$i] !== $receiver[$i]
+		// send mail to each donor
+	}
 }
